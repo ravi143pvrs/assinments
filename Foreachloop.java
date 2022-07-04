e32e73d5604336508760718ac669f98b804f235a
ravi143pvrs <107831877+ravi143pvrs@users.noreply.github.com>
2022-07-04 17:50:52 +0530

    Add files via upload

---
+++ Foreachloop.java
@@ -0,0 +1,12 @@
+package Assignmentss;
+
+public class Foreachloop {
+
+	public static void main(String[] args) {
+		int v[]= {23,34,45,56,67,78,89,12,90,01};
+		for(int a : v) {
+			System.out.println(a);
+			}
+	}
+
+}
